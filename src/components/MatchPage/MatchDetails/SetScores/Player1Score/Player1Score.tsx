f1878a152b64b919c7dea9fae878aa26048e70b3
paulyeo23 <78999327+paulyeo23@users.noreply.github.com>
2024-05-19 03:33:51 +0800

    Renamed alt

--- src/components/MatchPage/MatchDetails/SetScores/Player1Score/Player1Score.tsx
+++ src/components/MatchPage/MatchDetails/SetScores/Player1Score/Player1Score.tsx
@@ -4,9 +4,9 @@ export const Player1Score: React.FC = () => {
       <span className="match-page maps results results-left lost whitespace-nowrap overflow-hidden text-ellipsis flex-shrink-0 flex-grow-0 w-[43%] flex text-red-600">
         <div className="results-teamlogo-container flex-none h-5 w-5 flex items-center justify-center relative top-[3px] mx-[5px] ml-[2px]">
           <img
-            alt="fnatic"
+            alt="UK"
             className="logo max-h-full max-w-full h-auto w-auto border-none"
-            title="fnatic"
+            title="UK"
             height="20px"
             width="20px"
             src="https://www.hltv.org/img/static/flags/30x20/GB.gif"
